3015d8bf8830c346852cee5385fa0112a32066aa
GiacoPaltri <giaco.paltri@gmail.com>
2022-02-09 19:21:50 +0100

    Added functionality to send emails and small changes

--- src/main/java/org/lauchproject/prova.java
+++ src/main/java/org/lauchproject/prova.java
@@ -1,6 +1,5 @@
 package org.lauchproject;
 
-
 import java.io.*;
 import java.security.SecureRandom;
 import java.util.ArrayList;
@@ -9,7 +8,7 @@ import java.util.ListIterator;
 
 public class prova {
 
-/** This method takes a String containing one or more commands to execute in the CMD, the result String you get in return is the output of the command you would see on the CMD**/
+/** This method takes a String containing one or more commands (command -> to use more commands, just insert command1 && command2...) to execute in the CMD, the result String you get in return is the output of the command you would see on the CMD**/
     public static String executeCommand(String command) {
         String line;
         StringBuilder result = new StringBuilder();
@@ -31,13 +30,15 @@ public class prova {
             }
         } catch (IOException e) {
             System.out.println("Exception = " + e.getMessage());
+            System.out.println("an error occurred while reading the lines (r.readline)");
         } catch (InterruptedException e) {
             e.printStackTrace();
+            System.out.println("an error occurred while starting the builder(builder.start)");
             //waitFor error
         }
         return result.toString();
     }
-/** This function generates a random password of a specified length**/
+/** This function generates a random password of a specified length (len)**/
     public static String generateRandomPassword(int len) {
         // ASCII range – alphanumeric (0-9, a-z, A-Z)
         final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -60,14 +61,14 @@ public class prova {
     private static void generateCredentials(ArrayList<String> users) {
         String separator = System.getProperty("file.separator");
         String absolutePath = "C:" + separator + "Program Files" + separator + "mosquitto" + separator + "pwfile.txt";
-        ListIterator<String> iterator = users.listIterator();
         for (int i = 0; i < users.size(); i++){
             String pwd = generateRandomPassword(8);
+            SendMail.send(users.get(i),"prova", users.get(i)+":"+pwd);
             users.set(i, users.get(i)+":"+pwd);
         }
         writeToFile(absolutePath,users);
     }
-/** This function writes on a file which path has to be specified (including file name) in absolutePath (note that you have to use a separator, or 2 \\ -> not C:\...\file.txt BUT C:\\...\\file.txt). every line that has to be written has to be placed in an Arraylist element (lines)**/
+/** This function writes on a file which path has to be specified (including file name) in absolutePath (note that you have to use a separator, or 2 \\ -> NOT C:\...\file.txt BUT C:\\...\\file.txt). every line that has to be written has to be placed in an Arraylist element (lines)**/
     public static void writeToFile(String absolutePath, ArrayList<String> lines) {
         File file = new File(absolutePath); // Creates File object with the specified path. The path must include the filename
         if (!file.exists()) {
@@ -103,18 +104,16 @@ public class prova {
         }
     }
 /** This method sets ACL's for every user (topic restriction) **/
-    private static void setACLS() {}
+    private static void setACLs() {}
 
 /** Main method **/
     public static void main(String[] args) {
         ArrayList<String> userList = new ArrayList<String>();
-        userList.add("giaco.paltri@gmail.com");
-        userList.add("giaco1.paltri@gmail.com");
-        userList.add("giaco65.paltri@gmail.com");
+        userList.add("TRISSER.server@gmail.com");
+        //userList.add("abdullah.ali@einaudicorreggio.it"); // list of users
         generateCredentials(userList);
         System.out.println(executeCommand("cd C:\\Program Files\\mosquitto\\ && Net start Mosquitto")); // Starts the mosquitto broker
+        //new MQTTPubPrint(); // test send message
         System.out.println(executeCommand("Taskkill /IM \"mosquitto.exe\" /F")); // Closes the mosquitto broker
     }
-
-
 }
